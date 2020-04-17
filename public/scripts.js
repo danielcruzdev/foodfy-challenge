961ef6f29f8d27ee401719dd03e4a8843d0b3e0c
Daniel Cruz <d.afc98@gmail.com>
2020-04-17 14:11:17 -0300

    Desafio finalizado

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,23 @@
 const cards = document.querySelectorAll(".recipe-card");
+const details = document.querySelectorAll(".details-info");
 
 for (let card of cards) {
   card.addEventListener("click", () => {
     const receitaID = card.getAttribute("id");
     window.location.href = `/receitas/${receitaID}`;
   });
-};
+}
+
+for (let detail of details) {
+  let a = detail.querySelector("a");
+
+  a.addEventListener("click", function () {
+    if (detail.querySelector(".detail-content").classList.contains("hidden")) {
+      a.innerText = "ESCONDER";
+      detail.querySelector(".detail-content").classList.remove("hidden");
+    } else {
+      a.innerText = "MOSTRAR";
+      detail.querySelector(".detail-content").classList.add("hidden");
+    }
+  });
+}
