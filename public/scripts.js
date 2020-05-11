a44b64ddc6224879d38860c2fd83bb6a0b766c8f
Daniel Cruz <d.afc98@gmail.com>
2020-05-11 17:58:25 -0300

    Pagination done!

--- public/scripts.js
+++ public/scripts.js
@@ -1,61 +1,137 @@
 const cards = document.querySelectorAll(".recipe-card");
 const details = document.querySelectorAll(".details-info");
 
-for (let card of cards) {
-  card.addEventListener("click", () => {
-    const receitaID = card.getAttribute("id");
-    window.location.href = `/receitas/${receitaID}`;
-  });
+
+if(cards){
+  for (let card of cards) {
+    card.addEventListener("click", () => {
+      const receitaID = card.getAttribute("id");
+      window.location.href = `/receitas/${receitaID}`;
+    });
+  }
 }
 
-for (let detail of details) {
-  let a = detail.querySelector("a");
-
-  a.addEventListener("click", function () {
-    if (detail.querySelector(".detail-content").classList.contains("hidden")) {
-      a.innerText = "ESCONDER";
-      detail.querySelector(".detail-content").classList.remove("hidden");
-    } else {
-      a.innerText = "MOSTRAR";
-      detail.querySelector(".detail-content").classList.add("hidden");
-    }
-  });
+if(details){
+  for (let detail of details) {
+    let a = detail.querySelector("a");
+  
+    a.addEventListener("click", function () {
+      if (detail.querySelector(".detail-content").classList.contains("hidden")) {
+        a.innerText = "ESCONDER";
+        detail.querySelector(".detail-content").classList.remove("hidden");
+      } else {
+        a.innerText = "MOSTRAR";
+        detail.querySelector(".detail-content").classList.add("hidden");
+      }
+    });
+  }
 }
 
-function addIngredient() {
-  const ingredients = document.querySelector("#ingredients");
-  const fieldContainer = document.querySelectorAll(".ingredient");
+if(document.querySelector(".add-ingredient")){
 
-  // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
 
-  // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
-
-  // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
+  function addIngredient() {
+    const ingredients = document.querySelector("#ingredients");
+    const fieldContainer = document.querySelectorAll(".ingredient");
+  
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+  }
+  
+  document
+    .querySelector(".add-ingredient")
+    .addEventListener("click", addIngredient);
+  
+  function addPreparation() {
+    const preparation = document.querySelector("#preparation");
+    const fieldContainer = document.querySelectorAll(".preparation");
+  
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    preparation.appendChild(newField);
+  }
+  
+  document
+    .querySelector(".add-preparation")
+    .addEventListener("click", addPreparation);
+  
 }
 
-document
-  .querySelector(".add-ingredient")
-  .addEventListener("click", addIngredient);
 
-function addPreparation() {
-  const preparation = document.querySelector("#preparation");
-  const fieldContainer = document.querySelectorAll(".preparation");
+//Paginação
 
-  // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+function paginate(selectedPage, totalPages) {
+  let pages = [],
+      oldPage
 
-  // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-  // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  preparation.appendChild(newField);
+      const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+      const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+      const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+      if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+          if (oldPage && currentPage - oldPage > 2) {
+              pages.push('...')
+
+          }
+
+          if (oldPage && currentPage - oldPage == 2) {
+              pages.push(oldPage + 1)
+          }
+
+          pages.push(currentPage)
+
+          oldPage = currentPage
+
+      }
+  }
+
+  return pages
 }
 
-document
-  .querySelector(".add-preparation")
-  .addEventListener("click", addPreparation);
+const pagination = document.querySelector(".pagination")
+
+
+function createPagination(pagination){
+  const page = +pagination.dataset.page;
+  const total = +pagination.dataset.total;
+  const pages = paginate(page, total)
+  const filter = pagination.dataset.filter
+
+  let elements = ""
+
+  for (let page of pages) {
+      if (String(page).includes("...")) {
+          elements += `<span>${page}</span>`
+
+      } else {
+          if (filter) {
+              elements += `<a href="?page=${page}&filter=${filter}">${page} </a>`
+          }
+          else {
+              elements += `<a href="?page=${page}">${page}</a>`
+          }
+      }
+  }
+  pagination.innerHTML = elements
+}
+
+
+if (pagination) {
+ createPagination(pagination)
+}
--- src/app/controllers/recipes/main.js
+++ src/app/controllers/recipes/main.js
@@ -1,37 +1,53 @@
-const Recipe = require('../../models/Recipes')
-const { date } = require('../../../lib/utils')
+const Recipe = require("../../models/Recipes");
+const { date } = require("../../../lib/utils");
 
 module.exports = {
-    index(req, res) {
-        const { filter } = req.query
+  index(req, res) {
+    const { filter } = req.query;
 
-        if (filter) {
-            Recipe.findBy(filter, (recipes) => {
-                return res.render('main/recipes', { recipes })
-            })
-        } else {
-            Recipe.index((recipes) => {
-                return res.render('main/home', { recipes })
-            })
-        }
-
-    },
-    about(req, res) {
-        return res.render('main/about')
-    },
-    showAll(req, res) {
-        Recipe.all((recipes) => {         
-                return res.render('main/recipes', { recipes })
-        })
-    },
-    show(req, res) {
-        Recipe.find(req.params.id, (recipe) => {
-            if (!recipe) return res.send("Recipe not found!")
-
-            recipe.created_at = date(recipe.created_at).format
-
-            return res.render("main/recipe", { recipe })
-
-        })
+    if (filter) {
+      Recipe.findBy(filter, (recipes) => {
+        return res.render("main/recipes", { recipes });
+      });
+    } else {
+      Recipe.index((recipes) => {
+        return res.render("main/home", { recipes });
+      });
     }
-}
+  },
+  about(req, res) {
+    return res.render("main/about");
+  },
+  showAll(req, res) {
+    let { filter, page, limit } = req.query;
+
+    page = page || 1;
+    limit = limit || 6;
+    let offset = limit * (page - 1);
+
+    const params = {
+      filter,
+      page,
+      limit,
+      offset,
+      callback(recipes) {
+        const pagination = {
+          total: Math.ceil(recipes[0].total / limit),
+          page,
+        };
+        return res.render("main/recipes", { recipes, pagination, filter });
+      },
+    };
+
+    Recipe.paginate(params);
+  },
+  show(req, res) {
+    Recipe.find(req.params.id, (recipe) => {
+      if (!recipe) return res.send("Recipe not found!");
+
+      recipe.created_at = date(recipe.created_at).format;
+
+      return res.render("main/recipe", { recipe });
+    });
+  },
+};
